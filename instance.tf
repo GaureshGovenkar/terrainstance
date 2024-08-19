29326983dab06be2f283f49622c9ded999027b84
GaureshGovenkar <94797817+GaureshGovenkar@users.noreply.github.com>
2024-08-19 13:06:23 +0530

    Create instance.tf 1

---
+++ instance.tf
@@ -0,0 +1,20 @@
+resource "aws_instance" "docker" {
+    ami = "ami-0c8e23f950c7725b9"
+    instance_type = "t2.medium"
+    security_groups = ["My-traffic"]
+    key_name = "gaureshnew"
+    user_data = <<EOF
+    #!/bin/bash
+    sudo yum install git -y
+    sudo yum install java-17-amazon-corretto -y
+    sudo yum search docker
+    sudo yum info docker 
+    sudo yum install docker -y
+    sudo newgrp docker
+    sudo -a -G docker ec2-user
+    sudo enable --now docker
+    EOF
+    tags = {
+        Name = "docker"
+    }
+}
